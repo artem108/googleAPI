0c13220eb388861ea7eb9e27648e205ccfe9f70b
Artem Arshaniy <artem.arshaniy108@gmail.com>
2016-12-07 19:44:41 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function GoogleCallback(jqueryObj, data) {
 }
 (function() {
   $.ajax({
-        url:'http://ajax.googleapis.com/ajax/services/search/web?v1.0?key=AIzaSyCN13FKhms1ozL-hO3InUkJmNYQg8dzJWM=large&q=PHP&callback=GoogleCallback&context=?',
+        url:'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=AIzaSyCN13FKhms1ozL-hO3InUkJmNYQg8dzJWM=' +'test'+'&callback=GoogleCallback&context=?',
         dataType: 'jsonp'
 });
 });
